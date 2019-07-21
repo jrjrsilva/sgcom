179c96702a1d61ee88ab209bb834c46cabe61caf
Silva <jrjrsilva@gmail.com>
2019-07-21 02:01:46 -0300

    Ligando o Model User ao Model Efetivo

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace sgcom;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use sgcom\Models\Efetivo;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function efetivo(){
+        return $this->belongsTo(Efetivo::class);
+    }
+    
 }
